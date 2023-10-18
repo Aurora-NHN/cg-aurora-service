fd475182cd9526fcf802d3677c71f0cea160fc4e
mainhuitvt96 <quynhnhumaivtvn2012@gmail.com>
2023-10-18 09:22:10 +0700

    API free numerology report

--- src/main/java/com/codegym/aurora/payload/response/MiddleAgedNumberResponseDto.java
+++ src/main/java/com/codegym/aurora/payload/response/MiddleAgedNumberResponseDto.java
@@ -9,5 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MiddleAgedNumberResponseDto {
     private int number;
+    private String name;
+    private String title;
     private String description;
 }
